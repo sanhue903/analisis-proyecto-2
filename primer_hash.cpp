052277a5ae3fbac88c36efc4ca59b16c6c42381c
Lula Galvez <lulagalvez@github.com>
2022-06-02 17:43:28 -0400

    subido cpp de primer hash

---
+++ primer_hash.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include "hashT1.h"
+
+void hashT1::hashT1(std::vector<int> _keys)
+{
+    keys = _keys;
+}
+
+std::pair<int,int> hastT1::genAB(int p)
+{
+    
+}
