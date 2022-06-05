a7c5975ca42880f56ebcd3737b47362d43c85a00
Lula Galvez <lulagalvez@github.com>
2022-06-05 18:43:31 -0400

    agregado main que evita kmers repetidos

--- lula-main.cpp
+++ lula-main.cpp
@@ -5,22 +5,35 @@
 #include <vector>
 #include <random>
 #include <chrono>
+#include <set>
 #include "primer_hash.h"
 
 int main()
 {
     std::vector<std::string> vec;
+    std::set<std::string> st;
     std::ifstream file_in("genomas/100000-genomes.txt");
     if(!file_in) {}
 
     std::string s;
-    while(std::getline(file_in, s)) vec.push_back(s);
+    while(std::getline(file_in, s)) st.insert(s);
 
+    for(std::set<std::string>::iterator it = st.begin(); it != st.end(); it++)
+    {
+        vec.push_back(*it);
+    }
+    for(int i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << "\n";
+    }
+    std::cout << "size: " << vec.size() << "\n";
+    
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::default_random_engine generator(seed);
     std::uniform_int_distribution<int> distribution(0,PRIME - 1);
     primer_hash * ph = new primer_hash(distribution, generator, vec);
     ph->buildTable();
+    
 
     return 0;
 }
