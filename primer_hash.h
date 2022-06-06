075bfd02fc7b11bb29f1f73826d8c4db3dec38b6
Lula Galvez <67491339+lulagalvez@users.noreply.github.com>
2022-06-06 06:30:10 -0400

    Update primer_hash.h

--- primer_hash.h
+++ primer_hash.h
@@ -16,7 +16,10 @@ class primer_hash
         ~primer_hash();
         bool buildTable();
         void printTable();
+        std::string find(std::string s);
     private:
+        int avgTries2;
+        int tries1;
         std::pair<int,int> _ab;
         std::vector<std::pair<int,int> > ABs;
         std::uniform_int_distribution<int> distribution;
@@ -24,7 +27,7 @@ class primer_hash
         std::vector<std::vector<std::string> > table;
         std::vector<std::string> keys;
 
-        std::vector<std::string> buildTable2(std::vector<std::string> str_vr);
+        std::pair<std::vector<std::string>,std::pair<int,int> > buildTable2(std::vector<std::string> str_vr);
         std::pair<int,int> genAB(int p);
         int hashF(int p,std::pair<int,int> ab, int m, std::string key);
         int stringToInt(std::string key);
