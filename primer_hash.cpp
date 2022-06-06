e34f00dc2245e87d0c4db789745eb6c16408fcaa
Lula Galvez <67491339+lulagalvez@users.noreply.github.com>
2022-06-06 06:29:54 -0400

    Update primer_hash.cpp

--- primer_hash.cpp
+++ primer_hash.cpp
@@ -8,6 +8,9 @@ primer_hash::primer_hash(std::uniform_int_distribution<int> _distribution, std::
     distribution = _distribution;
     generator = _generator;
     keys = _keys;
+
+    avgTries2 = 0;
+    tries1 = 0;
 }
 
 bool primer_hash::buildTable()
@@ -27,29 +30,37 @@ bool primer_hash::buildTable()
         int sum = 0;
         for(int i = 0; i < test.size(); i++)  sum += pow(test[i],2);
 
-        // print statement here to see how many times we try to construct first level
+        tries1++;
         if(sum <= (4 * keys.size()))       
         {
             _ab = ab;
             for(int i = 0; i < buckets.size(); i++)
             {
-                table[i] = buildTable2(buckets[i]);
+                std::pair<std::vector<std::string>, std::pair<int,int> > pr = buildTable2(buckets[i]);
+                table[i] = pr.first;
+                ABs[i] = pr.second;
             }
             return true;
         }
     }
 }
 
-std::vector<std::string> primer_hash::buildTable2(std::vector<std::string> str_vr)
+std::pair<std::vector<std::string>,std::pair<int,int> > primer_hash::buildTable2(std::vector<std::string> str_vr)
 {
+    int avg = 1;
     int bucketSize = str_vr.size() * str_vr.size();
     std::vector<std::string> bucket(bucketSize,"_______");
 
-    if(str_vr.size() == 0) return bucket;
-    if(str_vr.size() == 1)
+    if(str_vr.size() == 0)           // Caso donde esta vacio, no es necesario calcular un ab
+    {
+        avgTries2 += avg;
+        return make_pair(bucket,std::make_pair(0,0));
+    }
+    if(str_vr.size() == 1)          // Caso donde solo hay 1 elemento, el tamano es 1 por tanto no se calcula ab
     {
         bucket[0] = str_vr[0];
-        return bucket;
+        avgTries2 += avg;
+        return make_pair(bucket,std::make_pair(0,0));
     }
 
     while(true)
@@ -67,17 +78,17 @@ std::vector<std::string> primer_hash::buildTable2(std::vector<std::string> str_v
             }
             else collisions[hash]++;
         }
-        
         if(flag)
         {
             for(int i = 0; i < str_vr.size(); i++)
             {
                 bucket[hashF(PRIME,ab,bucketSize,str_vr[i])] = str_vr[i];
+                avgTries2 += avg;
             }
-            break;
-        } 
+            return std::make_pair(bucket,ab);
+        }
+        avg++;
     }
-    return bucket;
 }
 
 std::pair<int,int> primer_hash::genAB(int p)
@@ -115,4 +126,30 @@ void primer_hash::printTable()
         for(int j = 0; j < table[i].size(); j++) std::cout << table[i][j] << " ";
         std::cout << std::endl;
     }
+    std::cout << "Tabla 1 creada despues de " << tries1 << " intentos" << std::endl;
+    std::cout << "Promedio de intentos de creacion de tabla 2: " << (double)avgTries2/table.size() << " intentos" << std::endl;
+    
 }
+
+std::string primer_hash::find(std::string s)
+{
+    int h1 = hashF(PRIME,_ab,table.size(),s);
+    if(table[h1].size() == 0)
+    {
+        std::cout << "No se encuentra" << std::endl;
+        return "";
+    }
+    if(table[h1].size() == 1)
+    {
+        std::cout << "Encontrado: " << table[h1][0] << " en posicion " << h1 << " " << 0 << std::endl;
+        return table[h1][0];
+    }
+    else
+    {
+        int h2 = hashF(PRIME,ABs[h1],table[h1].size(),s);
+        std::cout << "Encontrado: " << table[h1][h2] << " en posicion " << h1 << " " << h2 << std::endl;
+        return table[h1][h2];
+    }
+}
+
+
