7b47078dfc1dce487eb0c6e269242ee6f7ee4874
Lula Galvez <67491339+lulagalvez@users.noreply.github.com>
2022-06-06 06:29:36 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,18 @@
 #include <fstream>
 #include <vector>
 #include <set>
+#include <chrono>
 #include <ctime>
 #include "primer_hash.h"
 
 int main(){
-    srand(time(NULL));
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
+    std::uniform_int_distribution<int> distribution(0,PRIME - 1);
     
     std::vector<std::string> vec;
     std::set<std::string> st;
-    std::ifstream file_in("genomas/100-genomes.txt");
+    std::ifstream file_in("genomas/100000-genomes.txt");
     if(!file_in) {}
 
     std::string s;
@@ -20,12 +23,19 @@ int main(){
     {
         vec.push_back(*it);
     }
+    primer_hash * h = new primer_hash(distribution,generator,vec);
+    h->buildTable();
+    h->printTable();
+    h->find("CGACGCCGGTTCATC");
 
     /*
+    *   MEDICIONES DE TIEMPO:
+    *
     *   Para probar con distintos numeros de kmers importar archivos de distintos tamanos,
     *   abajo se calcula un promedio de construir la tabla y buscar un elemento n veces con
     *   el tamano del archivo de kmers importado.
     */
+    /*
     int n = 1000;
     double nano = 1000000000;//Variable usada para pasar a nanoSegundos
     double tiempoConstruir = 0, tiempoBuscar = 0;       
@@ -34,8 +44,9 @@ int main(){
     std::cout << " construir ; buscar " << std::endl;
     for(int i = 0; i < n; i++)
     {
-        hashT1 h = hashT1(vec);
-        h.imprimir();
+        primer_hash * h = new primer_hash(distribution,generator,vec);
+        h->buildTable();
+        h->printTable();
     }
     tiempoConstruir = ((double)std::clock() - start)/CLOCKS_PER_SEC;
 
@@ -47,5 +58,6 @@ int main(){
     tiempoBuscar = ((double)std::clock()-start)/CLOCKS_PER_SEC;
 
     std::cout << n << ";" << (tiempoConstruir/(double)n)*nano << ";" << (tiempoBuscar/(double)n)*nano << std::endl;
+    */
     return 0;
 }
